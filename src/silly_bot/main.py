c93dd8f8925bbe54061a78aad45575f2972fc6d4
Mikhail Kashin <144782206+Mikkkkkkka@users.noreply.github.com>
2026-03-08 13:36:01 +0300

    updated the forbidden words list

--- src/silly_bot/main.py
+++ src/silly_bot/main.py
@@ -61,8 +61,8 @@ async def handle_message(message: Message) -> None:
         expanded_word_with_multiple_endings("бисекс"),
         expanded_word_with_multiple_endings("транс"),
         expanded_word_with_multiple_endings("гомос"),
+        expanded_word_with_multiple_endings("пидор"),
         expanded_word_regex("квир"),
-        expanded_word_regex("пидор"),
         expanded_word_regex("лгбт"),
     ])(x) for x in message_words]):
         await forbidden_reply(message)
